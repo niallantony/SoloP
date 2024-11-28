3f5553b83ce2451389cfa0b825497c3b6d8384ab
Niall Antony <88824327+niallantony@users.noreply.github.com>
2024-11-28 22:17:24 +0900

    Update CLI

--- project_plan_manager/cli.py
+++ project_plan_manager/cli.py
@@ -1,17 +1,20 @@
 import argparse
-from plan_manager import add_task, delete_task
+from plan_manager import add_task, delete_task, change_file, change_status
 
 def main():
     parser = argparse.ArgumentParser(description="Task Manager")
     parser.add_argument('--add', type=str, help="Add a new task")
     parser.add_argument('--delete', type=int, help="Delete a task by ID")
+    parser.add_argument('--status', nargs=2, help="Change a status message, takes an ID and new status")
 
     args = parser.parse_args()
 
     if args.add:
-        add_task(args.add)
+        change_file(add_task, args.add)
     elif args.delete:
-        delete_task(args.delete)
+        change_file(delete_task, args.delete)
+    elif args.status:
+        change_file(change_status, int(args.status[0]), args.status[1])
         
 if __name__ == "__main__":
     main()
